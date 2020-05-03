ed89a320215da1a1bb606bdeceebdf133c34298c
Erika <ehowell20@punahou.edu>
2020-05-03 00:32:57 +0000

    method headers

---
+++ FractionCalculator.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class FractionCalculator
+{
+    public static void main(String[] args)
+    {
+        
+    }
+    // asks user for operation
+    public static String getOperation(Scanner operation)
+    {
+        
+    }
+    // return true if valid fraction
+    public static boolean validFraction(String fraction)
+    {
+        
+    }
+    // asks user for valid fraction
+    public static Fraction getFraction(Scanner fraction)
+    {
+        
+    }
+}
